6b2fa40450aa4a103501cf5410adddde03341048
Rahul Powar <rahul@redsift.io>
2015-11-04 21:27:04 +0000

    Added radial chart

---
+++ js/lang/lang.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Lang = {
+	localShortMonths: function(lang) {
+		if (lang === undefined) {
+			lang = navigator.language;
+		}
+		var formatter = null;
+		
+		if (window.Intl !== undefined) {
+			formatter = new Intl.DateTimeFormat(lang, {
+				month: "short"
+			});
+		} else {
+			formatter = {
+				format: function(date) {
+				return date.toUTCString().split(' ')[2]
+				}
+			}
+		}
+		return Array.apply(null, Array(12)).map(function(_, i) {
+			return formatter.format(new Date(2014, i));
+		});
+	}	
+};
+
+if (typeof module !== 'undefined' && module.exports) { module.exports = Lang; } // CommonJs export
+if (typeof define === 'function' && define.amd) { define([], function () { return Lang; }); } // AMD
