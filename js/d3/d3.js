74fed5a906865537c7ae62723cf1580c6bc517d4
Rahul Powar <rahul@redsift.io>
2016-03-01 17:32:30 +0000

    Updated angle

--- js/d3/d3.js
+++ js/d3/d3.js
@@ -10,20 +10,23 @@ var reusable = {
   legendChart: legendChart
 };
 
+var RedsfitAngle = 33.75;
+
 var D3 = {
   Tools: tools,
   Reusable: reusable,
   Components: components,
   Constants: {
+      Angle: RedsfitAngle,
       Patterns: {
         crosshatch1: { ang: 45, w: 4, h: 4, s: 5 },
         crosshatch2: { ang: 45, w: 3, h: 4, s: 5 }, 
         crosshatch3: { ang: 45, w: 3, h: 3, s: 5 },   
-        diagonal1: { ang: 30, w: 5, h: 4, s: 5 }, 
-        diagonal2: { ang: 30, w: 5, h: 3, s: 5 }, 
-        diagonal3: { ang: 30, w: 5, h: 2, s: 5 },
+        diagonal1: { ang: RedsfitAngle, w: 5, h: 4, s: 5 }, 
+        diagonal2: { ang: RedsfitAngle, w: 5, h: 3, s: 5 }, 
+        diagonal3: { ang: RedsfitAngle, w: 5, h: 2, s: 5 },
         blocks: { ang: 0, w: 3, h: 4, s: 5 },
-        redsift: { ang: 30, w: 3, h: 3, s: 5 },   
+        redsift: { ang: RedsfitAngle, w: 3, h: 3, s: 5 },   
     }
   }
 };
