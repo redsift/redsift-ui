065e3e52d4b27240bbc8732491afc6eb6c563b02
Rahul Powar <rahul@redsift.io>
2016-03-01 12:20:34 +0000

    Moved constants in

--- js/d3/d3.js
+++ js/d3/d3.js
@@ -13,7 +13,19 @@ var reusable = {
 var D3 = {
   Tools: tools,
   Reusable: reusable,
-  Components: components
+  Components: components,
+  Constants: {
+      Patterns: {
+        crosshatch1: { ang: 45, w: 4, h: 4, s: 5 },
+        crosshatch2: { ang: 45, w: 3, h: 4, s: 5 }, 
+        crosshatch3: { ang: 45, w: 3, h: 3, s: 5 },   
+        diagonal1: { ang: 30, w: 5, h: 4, s: 5 }, 
+        diagonal2: { ang: 30, w: 5, h: 3, s: 5 }, 
+        diagonal3: { ang: 30, w: 5, h: 2, s: 5 },
+        blocks: { ang: 0, w: 3, h: 4, s: 5 },
+        redsift: { ang: 30, w: 3, h: 3, s: 5 },   
+    }
+  }
 };
 
 if (typeof module !== 'undefined' && module.exports) { module.exports = D3; } // CommonJs export
