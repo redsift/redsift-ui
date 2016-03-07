85e358c62711f8fb1285d14b44e81d357f36be1f
Rahul Powar <rahul@redsift.io>
2016-03-07 17:58:24 +0000

    Added pie chart

---
+++ js/d3/pieChart.js
@@ -0,0 +1,95 @@
+/* global d3 */
+
+'use strict';
+
+var tools = require('./tools.js');
+
+function pieChart() {
+  var outerRadius = 100, 
+    innerRadius = 0, 
+    classed = 'pie', 
+    fill = null,
+    animationEnd = null,
+    animationDuration = tools.redsiftDuration(),
+    animation = tools.redsiftEasing();
+  
+  function impl(selection) {
+    var arc = d3.svg.arc()
+                .outerRadius(outerRadius)
+                .innerRadius(innerRadius);
+    
+
+                            
+    selection.each(function(data) {
+               
+        var p = d3.select(this).selectAll('path.'+classed)
+            .data(data);
+            
+        p.enter().append('path')
+            .attr('class', classed);
+        p.exit().remove();
+        
+        if (animation) {
+            p.transition().ease(animation).duration(animationDuration).attr('d', arc).each('end', function() {
+                if (animationEnd) {
+                    animationEnd();
+                }
+            });     
+        } else {
+            p.attr('d', arc);     
+        }
+            
+        
+        if (fill) {
+            p.style('fill', fill);
+        }      
+    });
+  }
+
+  impl.animationEnd = function(value) {
+    if (!arguments.length) return animationEnd;
+    animationEnd = value;
+    return impl;
+  };
+  
+  impl.animation = function(value) {
+    if (!arguments.length) return animation;
+    animation = value;
+    return impl;
+  };
+
+  impl.animationDuration = function(value) {
+    if (!arguments.length) return animationDuration;
+    animationDuration = value;
+    return impl;
+  };
+
+  impl.fill = function(value) {
+    if (!arguments.length) return fill;
+    fill = value;
+    return impl;
+  }; 
+
+  impl.classed = function(value) {
+    if (!arguments.length) return classed;
+    classed = value;
+    return impl;
+  }; 
+
+  impl.outerRadius = function(value) {
+    if (!arguments.length) return outerRadius;
+    outerRadius = value;
+    return impl;
+  };  
+  
+  impl.innerRadius = function(value) {
+    if (!arguments.length) return innerRadius;
+    innerRadius = value;
+    return impl;
+  };    
+  
+  return impl;
+}
+
+if (typeof module !== 'undefined' && module.exports) { module.exports = pieChart; } // CommonJs export
+if (typeof define === 'function' && define.amd) { define([], function () { return pieChart; }); } // AMD
