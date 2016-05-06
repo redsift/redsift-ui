22f547ff6895f4a6009ce846a8c61e245ab5d10c
Martin Hecher <martin@redsift.io>
2016-05-06 15:32:19 +0100

    added 'rollup' task

--- gulpfile.js
+++ gulpfile.js
@@ -17,20 +17,22 @@ var closureCompiler = require('gulp-closure-compiler');
 var browserSync = require('browser-sync').create();
 var plumber = require('gulp-plumber');
 var streamqueue = require('streamqueue');
+var spawn = require('child_process').spawn;
 
 
 // Clean
 gulp.task('clean', function() {
-    return del([ 'distribution/**' ]);
+    return del(['distribution/**']);
 });
 
 gulp.task('js', function() {
-    return streamqueue(
-            { objectMode: true },
+    return streamqueue({
+                objectMode: true
+            },
             gulp.src('./node_modules/babel-polyfill/browser.js'),
             browserify(['./js/redsift.js'])
-              .bundle()
-              .pipe(source('./js/redsift-browserify.js')))
+            .bundle()
+            .pipe(source('./js/redsift-browserify.js')))
         .pipe(buffer())
         .pipe(concat('redsift-global.js'))
         .pipe(plumber())
@@ -38,7 +40,9 @@ gulp.task('js', function() {
         .pipe(babel({
             presets: ['es2015']
         }))
-        .pipe(rename({suffix: '.es5'}))
+        .pipe(rename({
+            suffix: '.es5'
+        }))
         .pipe(gulp.dest('./distribution/js'))
         .pipe(closureCompiler({
             compilerPath: 'bower_components/closure-compiler/compiler.jar',
@@ -46,6 +50,22 @@ gulp.task('js', function() {
             continueWithWarnings: true
         }))
         .pipe(gulp.dest('./distribution/js'));
+});
+
+gulp.task('rollup', function() {
+    var child = spawn("rollup", ["-c"], {
+        cwd: process.cwd()
+    });
+
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function(data) {
+        console.log(data.toString());
+    });
+
+    child.stderr.setEncoding('utf8');
+    child.stderr.on('data', function(data) {
+        console.log(data.toString());
+    });
 });
 
 function makeCss(name) {
@@ -64,12 +84,18 @@ function makeCss(name) {
         }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./distribution/css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifyCss({compatibility: '*', roundingPrecision: 4, keepSpecialComments: 0}))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifyCss({
+            compatibility: '*',
+            roundingPrecision: 4,
+            keepSpecialComments: 0
+        }))
         .pipe(sourcemaps.write('../../distribution/maps'))
         .pipe(gulp.dest('./distribution/css'))
         .pipe(browserSync.stream())
-        .on('error', function (e) {
+        .on('error', function(e) {
             console.error(e.message);
         });
 }
@@ -82,47 +108,55 @@ function makeCss(name) {
 // where the given path is a folder which meteor will reload automatically on a
 // file change.
 function appRefresh() {
-  var fs = require('fs'),
-      path = require('path');
+    var fs = require('fs'),
+        path = require('path');
 
-  var configFilePath = path.join(__dirname, 'trigger-app-reload.json'),
-  configFile = null;
+    var configFilePath = path.join(__dirname, 'trigger-app-reload.json'),
+        configFile = null;
 
-  try {
-    configFile = fs.readFileSync(configFilePath);
-  } catch(err) {
-    console.log('No config file "trigger-app-reload.json" found, skipping reloading trigger...');
-  }
-
-  if (configFile) {
-    var config = JSON.parse(configFile);
-
-    if (config && config.watchedFolder) {
-      var now = Date.now(),
-      outputFilePath = path.join(config.watchedFolder, 'ignore-me-from-redsift-ui.js'),
-      content = 'var now = ' + now + ';';
-
-      fs.writeFile(outputFilePath, content, function() {
-        console.log('Triggered application reload via "%s"...', outputFilePath);
-      });
+    try {
+        configFile = fs.readFileSync(configFilePath);
+    } catch (err) {
+        console.log('No config file "trigger-app-reload.json" found, skipping reloading trigger...');
     }
-  }
+
+    if (configFile) {
+        var config = JSON.parse(configFile);
+
+        if (config && config.watchedFolder) {
+            var now = Date.now(),
+                outputFilePath = path.join(config.watchedFolder, 'ignore-me-from-redsift-ui.js'),
+                content = 'var now = ' + now + ';';
+
+            fs.writeFile(outputFilePath, content, function() {
+                console.log('Triggered application reload via "%s"...', outputFilePath);
+            });
+        }
+    }
 }
 
-gulp.task('css-light', function () {
+gulp.task('css-light', function() {
     return makeCss('redsift-light');
 });
 
-gulp.task('css-dark', function () {
+gulp.task('css-dark', function() {
     return makeCss('redsift-dark');
 });
 
-gulp.task('css-xtra', function () {
+gulp.task('css-xtra', function() {
     return makeCss('redsift-xtra');
 });
 
 gulp.task('css', ['css-light', 'css-dark', 'css-xtra'], function() {
-  return appRefresh();
+    return appRefresh();
+});
+
+gulp.task('js-watch', ['js'], function() {
+    browserSync.reload('*.js');
+});
+
+gulp.task('es6-watch', ['rollup'], function() {
+    browserSync.reload('*.js');
 });
 
 gulp.task('browser-sync', function() {
@@ -134,18 +168,13 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('modules', function() {
-  return gulp.src([
-      './js/color/color.js'
-    ]).pipe(gulp.dest('./distribution/modules'));
-});
-
-gulp.task('js-watch', ['js'], function() { browserSync.reload('*.js'); });
-
 gulp.task('serve', ['default', 'browser-sync'], function() {
     gulp.watch(['./css/**/*.{import.styl,styl,css}'], ['css']);
+    gulp.watch(['./src/**/*.{js, tmpl, styl}'], ['es6-watch']);
     gulp.watch(['./js/**/*.js'], ['js-watch']);
-    gulp.watch('./samples/**/*.html').on('change', function() { browserSync.reload('*.html'); });
+    gulp.watch('./samples/**/*.html').on('change', function() {
+        browserSync.reload('*.html');
+    });
 });
 
-gulp.task('default', ['css', 'js', 'modules']);
+gulp.task('default', ['css', 'rollup', 'js']);
