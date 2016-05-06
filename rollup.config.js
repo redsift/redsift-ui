9774cccfbdc713d416c38f3912d8e2acf0dc8b3d
Martin Hecher <martin@redsift.io>
2016-05-06 15:38:36 +0100

    added string loading template to rollup. create 'umd' packages, not 'cjs'

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,27 @@ import json from 'rollup-plugin-json';
 // import babel from 'rollup-plugin-babel';
 import buble from 'rollup-plugin-buble';
 import stylusCssModules from 'rollup-plugin-stylus-css-modules';
+import string from 'rollup-plugin-string';
+
+import path from 'path';
+
+const distRootPath = './dist'; // TODO: use 'config' package for central configuration
 
 export default {
     entry: 'src/index.js',
-    format: 'cjs',
-    plugins: [stylusCssModules({
-        output: 'dist/redsift-ui.css'
-    }), json(), buble()],
-    // }), json(), babel()],
-    dest: 'dist/redsift-ui.umd.js'
+    format: 'umd',
+    plugins: [
+        json(),
+        stylusCssModules({
+            output: path.join(distRootPath, 'redsift-ui.css')
+        }),
+        string({
+            extensions: ['.tmpl']
+        }),
+        // CAUTION: make sure to initialize all additional plugins BEFORE babel()
+        // buble(). Otherwise the transpiler will consume the imported files first.
+        // babel(),
+        buble()
+    ],
+    dest: path.join(distRootPath, 'redsift-ui.umd.js')
 };
