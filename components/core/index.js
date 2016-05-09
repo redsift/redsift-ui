b944119145d9febd791bd85d14534623a0e86c9e
Martin Hecher <martin@redsift.io>
2016-05-09 16:30:50 +0100

    ported 'Widgets' to ES6

--- components/core/index.js
+++ components/core/index.js
@@ -3,3 +3,8 @@ export { ColorTools } from './src/js/color/index';
 export { Lang } from './src/js/lang/index';
 export { Scroll } from './src/js/scroll/index';
 export { Timing } from './src/js/timing/index';
+export { TreoWriter } from './src/js/treo-writer/index';
+export {
+	Sliders,
+	StyleSwitcher
+} from './src/js/widgets/index';
