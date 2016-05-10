11b706532296a0c446a09e3676432f512cc3dd1e
Martin Hecher <martin.hecher@fraunhofer.at>
2016-05-10 19:04:20 +0100

    added 'hero' element to core bundle

--- bundles/core/index.js
+++ bundles/core/index.js
@@ -1,3 +1,8 @@
 export * from '../../components/core/index.js';
 
 export { svg } from '../../components/d3-rs-svg/index'
+
+export {
+    registerHeroElement,
+    RedsiftHero
+} from '../../components/hero/index.js';
--- bundles/full/index.js
+++ bundles/full/index.js
@@ -1,11 +1,6 @@
 export * from '../core/index';
 
 export {
-    registerHeroElement,
-    RedsiftHero
-} from '../../components/hero/index.js';
-
-export {
     registerRadialChartElement,
     RedsiftRadialChart
 } from '../../components/radial-chart/index.js';
