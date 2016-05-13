1de124fc14f5f550ce2d8008950c364a9419d345
Martin Hecher <martin@redsift.io>
2016-05-13 18:07:02 +0100

    added 'schedule' custom element

--- bundles/full/_components.js
+++ bundles/full/_components.js
@@ -1,9 +1,11 @@
 // Note: registering custom elements by default:
 import { registerHeroElement } from '../../components/hero/index';
 import { registerRadialChartElement } from '../../components/radial-chart/index';
+import { registerScheduleElement } from '../../components/schedule/index';
 
 registerHeroElement();
 registerRadialChartElement();
+registerScheduleElement();
 
 export {
     registerHeroElement,
@@ -14,3 +16,8 @@ export {
     registerRadialChartElement,
     RedsiftRadialChart
 } from '../../components/radial-chart/index.js';
+
+export {
+    registerScheduleElement,
+    RedsiftSchedule
+} from '../../components/schedule/index.js';
---
+++ components/schedule/index.js
@@ -0,0 +1,7 @@
+import registerScheduleElement from './src/js/schedule-element.js';
+import RedsiftSchedule from './src/js/schedule.js';
+
+export {
+  registerScheduleElement,
+  RedsiftSchedule
+}
